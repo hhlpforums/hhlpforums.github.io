3563b67034daad80eda47981543038bfd41304cc
hhlpforums <121887782+hhlpforums@users.noreply.github.com>
2023-01-03 13:43:50 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.getElementById("login").innerHTML = "Error: Usuario o contraseña incorrectos";
+}
---
+++ searchscript.js
@@ -0,0 +1,3 @@
+function myFunction() {
+  document.getElementById("Search").innerHTML = "Error al conectar con servidor";
+}
