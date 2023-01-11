c1567679628b64af96c1f455b50a1becf472d455
hhlpforums <121887782+hhlpforums@users.noreply.github.com>
2023-01-11 15:02:45 -0300

    Update searchscript.js

--- searchscript.js
+++ searchscript.js
@@ -1,6 +1,6 @@
 function myFunction() {
 	if(document.getElementById("sbar").value == "hack door"){
-		location.replace("https:hhlpforums.github.io/dh2.html");
+		window.location.replace("/dh2.html");
 	}else{
   document.getElementById("Search").innerHTML = "Error al conectar con servidor";
 }
